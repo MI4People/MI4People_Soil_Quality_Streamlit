45521c0bbbe5263f5addfd05e82459141c291099
Paul Springer <springer.paul@gmx.net>
2022-01-17 10:11:55 +0100

    Better Description in sidebar

--- prediction_page.py
+++ prediction_page.py
@@ -115,7 +115,7 @@ def show_predict_page():
     with col1:
         st.title("Organic Carbon Prediction")
     with col2:
-        st.image('Logo_with_Title.png', width=250)
+        st.image('Logo_with_Title.png', width=150)
 
     
     # some instructions for the user
